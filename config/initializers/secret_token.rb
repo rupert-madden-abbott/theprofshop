80f02ce1174896aeb347c7cc37acad9a6bd9b0cf
rupert654 <rupert654@gmail.com>
2010-12-08 15:23:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theprofshop::Application.config.secret_token = '6774f33ea9f538e15eb5ea913fe20b3224d1a984790f7b8a8f5d466b092e1fade02ee507b4988f667b6719f833e7b1bc8bbfa7fc67444f27892737943dab3d41'
