a46b25bde2e5a656bc5b975492a7d1c95593ace5
rupert654 <rupert654@gmail.com>
2011-01-08 05:02:03 +0000

    * Added History management to AJAX features * AJAX now only enabled in browsers that support History API

--- app/scripts/application.coffee
+++ app/scripts/application.coffee
@@ -1,9 +1,13 @@
-$ ->
-  $('#pages th a, #pages .pagination a').live 'click', ->
-    $.getScript(@href)
-    false
-  $('#pages_search input').keyup ->
-    $.get($('#pages_search').attr('action'), $('#pages_search').serialize(), null, 'script')
-    false
-
+if history && history.pushState
+  $ ->
+    $('#pages th a, #pages .pagination a').live 'click', ->
+      $.getScript(@href)
+      history.pushState(null, document.title, @href)
+      false
+    $('#pages_search input').keyup ->
+      $.get($('#pages_search').attr('action'), $('#pages_search').serialize(), null, 'script')
+      history.replaceState(null, document.title, $("#pages_search").attr("action") + "?" + $("#pages_search").serialize())
+      false
+    $(window).bind "popstate", ->
+      $.getScript(location.href)
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,23 @@
-/* DO NOT MODIFY. This file was compiled Sat, 08 Jan 2011 04:09:05 GMT from
+/* DO NOT MODIFY. This file was compiled Sat, 08 Jan 2011 04:52:58 GMT from
  * /home/rupert/projects/ruby/theprofshop/app/scripts/application.coffee
  */
 
 (function() {
-  $(function() {
-    $('#pages th a, #pages .pagination a').live('click', function() {
-      $.getScript(this.href);
-      return false;
+  if (history && history.pushState) {
+    $(function() {
+      $('#pages th a, #pages .pagination a').live('click', function() {
+        $.getScript(this.href);
+        history.pushState(null, document.title, this.href);
+        return false;
+      });
+      $('#pages_search input').keyup(function() {
+        $.get($('#pages_search').attr('action'), $('#pages_search').serialize(), null, 'script');
+        history.replaceState(null, document.title, $("#pages_search").attr("action") + "?" + $("#pages_search").serialize());
+        return false;
+      });
+      return $(window).bind("popstate", function() {
+        return $.getScript(location.href);
+      });
     });
-    return $('#pages_search input').keyup(function() {
-      $.get($('#pages_search').attr('action'), $('#pages_search').serialize(), null, 'script');
-      return false;
-    });
-  });
+  }
 }).call(this);
